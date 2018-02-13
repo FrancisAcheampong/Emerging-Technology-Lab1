a019b55f86d0f5e2f6ab2c437d66d191e2baf908
Francis Acheampong <francisacheampong@gmail.com>
2018-02-12 19:20:57 -0500

    ready for navi

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,3 +1,7 @@
 // require
+let index = require('../views/index');
+let feedback =require('./feedback');
+let thanku = require('./thankyou')
+
 
 // module.exports which points to a series of functions
